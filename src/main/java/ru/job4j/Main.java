b5221a3c837ef246d1f6e5df1e4cb539a998c867
Sergei Baranov <mariosb84@mail.ru>
2023-03-12 19:31:59 +0300

    4. TODO list [#3786]

---
+++ src/main/java/ru/job4j/Main.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class Main {
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+}
